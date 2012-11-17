5d1102499bcc8f087a43527145fe68d7eda7efe7
Jeff Escalante <hello@jenius.me>
2012-11-17 16:06:33 -0500

    better packaging as a stylus plugin

--- roots.js
+++ roots.js
@@ -1,8 +1,5 @@
-module.exports.version = '0.1.2';
-module.exports.path = __dirname;
-
-function plugin() {
-  return function(style){ 
+module.exports = function() {
+  return function(style){
     style.include(__dirname);
     style.import('roots-css')
   }
