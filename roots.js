f1ad34ccd814cd8393f750aa340cee3388cd1d1d
Jeff Escalante <hello@jenius.me>
2012-11-17 14:04:23 -0500

    return plugin so it can be 'use'd

--- roots.js
+++ roots.js
@@ -1,3 +1,10 @@
 module.exports = exports;
 exports.version = '0.1.0';
 exports.path = __dirname;
+
+function plugin() {
+  return function(style){ 
+    style.include(__dirname);
+    style.import('roots-css')
+  }
+}
