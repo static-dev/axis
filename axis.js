aa2e4f3944ff5c9c0ff9b1dfc4bfa450cfb995c7
Jeff Escalante <hello@jenius.me>
2013-10-14 15:05:15 -0400

    integrate autoprefixer
    - remove redundant vendor functionality

--- axis.js
+++ axis.js
@@ -1,7 +1,17 @@
+var autoprefixer = require('autoprefixer-stylus');
+
 module.exports = function(opts) {
   var implicit = (opts && opts.implicit == false) ? false : true;
+
   return function(style){
+    // include axis
     style.include(__dirname);
+
+    // implicit import handling
     if (implicit) style.import('axis');
+
+    // autoprefixer integration
+    autoprefixer()(style);
   }
+
 }
