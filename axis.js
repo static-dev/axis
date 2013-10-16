ef18d3e7062001123f6f54480de81f6c009eb003
Jeff Escalante <hello@jenius.me>
2013-10-15 23:07:57 -0400

    nib integration

--- axis.js
+++ axis.js
@@ -1,14 +1,27 @@
-var autoprefixer = require('autoprefixer-stylus');
+var autoprefixer = require('autoprefixer-stylus'),
+    nib = require('nib'),
+    path = require('path');
 
-module.exports = function(opts) {
+exports = module.exports = function(opts) {
   var implicit = (opts && opts.implicit == false) ? false : true;
 
   return function(style){
+    // include nib
+    nib()(style);
+
     // include axis
     style.include(__dirname);
 
     // implicit import handling
-    if (implicit) style.import('axis');
+    if (implicit) {
+      style.import('nib');
+      style.import('axis');
+    }
+
+    // autoprefixer integration
+    autoprefixer()(style);
   }
 
 }
+
+exports.path = __dirname;
